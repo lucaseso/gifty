86418e3a93069920b01d60367da87e7e6f9d5397
= <=>
2020-07-27 16:54:32 -0300

    Gifty for production

---
+++ precache-manifest.4ffd067be33e3fcc6de97734b6b23aa4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c46d1a4b5172c8e7e4",
+    "url": "/css/app.9c2e4f10.css"
+  },
+  {
+    "revision": "69aff1a2c48bb717c10d",
+    "url": "/css/chunk-vendors.f990211a.css"
+  },
+  {
+    "revision": "9faf0c3c6d13f9ac17e0440d51fe42b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05c46d1a4b5172c8e7e4",
+    "url": "/js/app.2089fc15.js"
+  },
+  {
+    "revision": "69aff1a2c48bb717c10d",
+    "url": "/js/chunk-vendors.3189b77e.js"
+  },
+  {
+    "revision": "e4f95ab2f80b3c11e63f713f71b76395",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
